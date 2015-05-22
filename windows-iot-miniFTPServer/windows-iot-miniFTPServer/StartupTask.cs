fa78ab43bf6d1fcce9a7b4664dedb98980ffc481
Kim Dam Development <kim@kimdamdev.dk>
2015-05-22 23:32:16 +0200

    FTP Server - cant compile. Need changes to Windows Store API

--- windows-iot-miniFTPServer/windows-iot-miniFTPServer/StartupTask.cs
+++ windows-iot-miniFTPServer/windows-iot-miniFTPServer/StartupTask.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net.Http;
+using System.Net;
 using Windows.ApplicationModel.Background;
+using SharpFtpServer;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -13,9 +15,13 @@ namespace windowsiotminiFTPServer
     {
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to start one or more asynchronous methods 
-            //
+            using (FtpServer server = new FtpServer(IPAddress.IPv6Any, 21))
+            {
+                server.Start();
+
+                Console.WriteLine("Press any key to stop...");
+                Console.ReadKey(true);
+            }
         }
     }
 }
